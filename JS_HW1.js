16930402b0b37eb9dc880d62f2286971f9c82532
Dzmitry Shkel <shkeldzima@gmail.com>
2023-03-28 15:19:04 +0200

    JS

---
+++ JS_HW1.js
@@ -0,0 +1,98 @@
+// 1. Create a variable “item_1”
+let item_1;
+
+// 2. Assign the variable item_1 the number 5
+item_1 = 5;
+
+// 3. Output item_1 to the console
+console.log(item_1);
+
+// 4. Create variable “item_2”
+let item_2;
+
+// 5. Assign the variable item_2 the number 3
+item_2 = 3;
+
+// 6. Output item_2 to the console
+console.log(item_2);
+
+// 7. Create variable “item_3”
+let item_3;
+
+// 8. Assign the item_3 variable the addition of item_1 and item_2
+item_3 = item_1 + item_2;
+
+// 9. Output item_3 to the console
+console.log(item_3);
+
+// 10. Create variable “item_4”
+let item_4;
+
+// 11. Assign the string “Yolochka” to the variable item_4
+item_4 = "Yolochka";
+
+// 12. Output item_4 to the console
+console.log(item_4);
+
+// 13. Display the addition of item_3 and item_4 to the console
+console.log(item_3 + item_4);
+
+// 14. Print the multiplication of item_3 and item_4 to the console
+console.log(item_3 * item_4);
+
+// 15. Create variable “item_5”
+let item_5;
+
+// 16. Assign item_5 to item_3
+item_5 = item_3;
+
+// 17. Create variable item_6
+let item_6;
+
+// 18. Create variable item_6_type
+let item_6_type;
+
+// 19. Set variable item_6 to 15
+item_6 = 15;
+
+// 20. Assign item_6_type variable the type of item_6 variable
+item_6_type = typeof item_6;
+
+// 21. Print the item_6 data type to the console as —— “item_6 == ” item_6, “item_6_type == ” item_6_type 
+console.log("item_6 == " + item_6 + ", item_6_type == " + item_6_type);
+
+// 22. Create variable item_7 and convert item_6 to String in it
+let item_7 = item_6.toString();
+
+// 23. Create variable item_7_type
+let item_7_type;
+
+// 24. Assign item_7_type variable the type of item_7 variable
+item_7_type = typeof item_7;
+
+// 25. Print the item_7 data type to the console as —— “item_7 == ” item_7, “item_7_type == ” item_7_type
+console.log("item_7 == " + item_7 + ", item_7_type == " + item_7_type);
+
+// 26. Create a variable "age_1" and assign the value 10 to it
+let age_1 = 10;
+
+// 27. Create variable “age_2” and assign value 18 to it
+let age_2 = 18;
+
+// 28. Create variable “age_3” and assign value 60 to it
+let age_3 = 60;
+
+// 29. Create an if in which you will check the value of the variable age_1
+// 30. If age_1 < age_2, print to console “You don’t have access cause your age is” + age_1 + “It’s less then”
+// 31. If age_1 >= age_2 and age_1 < age_3, output “Welcome!”
+// 32. If age_1 > age_3, output “Keep calm and look Culture channel” to the console
+// 33. Otherwise print "Technical work"
+if (age_1 < age_2) {
+console.log("You don’t have access cause your age is " + age_1 + " It’s less then " + age_2);
+} else if (age_1 >= age_2 && age_1 < age_3) {
+console.log("Welcome!");
+} else if (age_1 > age_3) {
+console.log("Keep calm and look Culture channel");
+} else {
+console.log("Technical work");
+}
